092547e33ac13bb7ae38782acd1dac11b87bcedd
Navratan Soni <navratans@adobe.com>
2024-08-07 14:41:49 +0530

    Used aep-library plugin

--- code/aepcomposeui/build.gradle.kts
+++ code/aepcomposeui/build.gradle.kts
@@ -1,43 +1,34 @@
 plugins {
-    id("com.android.library")
-    id("org.jetbrains.kotlin.android")
+    id("aep-library")
 }
 
-android {
+val mavenCoreVersion: String by project
+val aepComposeUiModuleName: String by project
+val aepComposeUiVersion: String by project
+val aepComposeUiMavenRepoName: String by project
+val aepComposeUiMavenRepoDescription: String by project
+
+aepLibrary {
     namespace = "com.adobe.marketing.mobile.aepcomposeui"
-    compileSdk = 34
 
-    defaultConfig {
-        minSdk = 24
+    moduleName = aepComposeUiModuleName
+    moduleVersion = aepComposeUiVersion
+    enableSpotless = true
+    enableCheckStyle = true
+    enableDokkaDoc = true
 
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
+    publishing {
+        mavenRepoName = aepComposeUiMavenRepoName
+        mavenRepoDescription = aepComposeUiMavenRepoDescription
+        gitRepoName = "aepsdk-ui-android"
+        addCoreDependency(mavenCoreVersion)
     }
 }
 
 dependencies {
-
-    implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("com.google.android.material:material:1.12.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.2.1")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("com.adobe.marketing.mobile:core:$mavenCoreVersion")
+    testImplementation("org.robolectric:robolectric:4.7")
+    testImplementation("io.mockk:mockk:1.13.11")
 }
+
+
