c5370ea11cacbe3cec8262324545a142ddc25cc5
Navratan Soni <navratans@adobe.com>
2024-09-26 22:09:33 +0530

    Added classes AepUiTemplate

---
+++ code/aepuitemplates/src/main/java/com/adobe/marketing/mobile/aepuitemplates/AepUiTemplate.kt
@@ -0,0 +1,5 @@
+package com.adobe.marketing.mobile.aepuitemplates
+
+interface AepUiTemplate {
+    fun getType(): String
+}
---
+++ code/aepuitemplates/src/main/java/com/adobe/marketing/mobile/aepuitemplates/utils/AepUiTemplateType.kt
@@ -0,0 +1,8 @@
+package com.adobe.marketing.mobile.aepuitemplates.utils
+
+enum class AepUiTemplateType(val typeName: String) {
+
+    SMALL_IMAGE("SmallImage"),
+    LARGE_IMAGE("LargeImage")
+
+}
